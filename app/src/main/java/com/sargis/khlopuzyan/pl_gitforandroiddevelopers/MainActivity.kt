3ba994c4deda12e8604c60d4fb19ca74d445f2a5
SargisKhlopuzyan1989 <skholpuzyan@shophq.com>
2021-11-18 09:21:49 +0400

    This is our second commit

--- app/src/main/java/com/sargis/khlopuzyan/pl_gitforandroiddevelopers/MainActivity.kt
+++ app/src/main/java/com/sargis/khlopuzyan/pl_gitforandroiddevelopers/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.sargis.khlopuzyan.pl_gitforandroiddevelopers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("THIS IS CONTAINED IN MY SECOND COMMIT")
     }
 }
